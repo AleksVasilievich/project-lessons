a2049a67f30de05a03536e3ae7ab05cdb352503b
Aleksandr <b.a.v.7575@mail.ru>
2022-05-29 22:31:10 +0300

    Add Tasks(25,27,29) lesson 4

---
+++ Task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число -> ");
+int num = int.Parse(Console.ReadLine());
+int lengthNum = Convert.ToString(num).Length;
+int sum = 0;
+int less1Num = 0;
+int i = 0;
+while (i < lengthNum)
+{
+    less1Num = num - num % 10;                                   
+    sum = sum + (num - less1Num);
+    num = num / 10;
+    i++;
+}
+Console.Write($"Сумма цифр заданного числа -> {sum} ");
+
