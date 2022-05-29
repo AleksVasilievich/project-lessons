37e263ee34e4111d9563462005dac01ff0f3a96e
Aleksandr <b.a.v.7575@mail.ru>
2022-05-29 21:48:08 +0300

    Add error correction Task_19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -4,32 +4,15 @@
 
 //   Вариант №1.
 
-// Console.Write(" Введите пятизначное число -> ");
-// string num = (Console.ReadLine());
-// if (num.Length != 5)
-// {
-//     Console.Write($" Ваше число : {num} - не пятизначное. ");
-//     return;
-// }
-
-// if (num[0] == num[4] || num[1] == num[3])
-// {
-//     Console.Write($" Ваше число : {num} -  Палиндром. ");
-// }
-// else Console.Write($" Ваше число : {num} - Не палиндром. ");
-
-// Вариант №2.
-
 Console.Write(" Введите пятизначное число -> ");
-var num = Convert.ToInt32(Console.ReadLine());
-string numText = Convert.ToString(num);
-if (numText.Length != 5)
+string num = (Console.ReadLine());
+if (num.Length != 5)
 {
     Console.Write($" Ваше число : {num} - не пятизначное. ");
     return;
 }
 
-if (numText[0] == numText[4] || numText[1] == numText[3])
+if (num[0] == num[4] && num[1] == num[3])
 {
     Console.Write($" Ваше число : {num} -  Палиндром. ");
 }
