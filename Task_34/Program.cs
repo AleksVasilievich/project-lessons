359aba9ee12abfbba83d8d18e335ada1eec74ad6
Aleksandr <b.a.v.7575@mail.ru>
2022-05-29 23:28:08 +0300

    Add Task_34 (edit)

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -7,7 +7,6 @@ Console.Write("Введите размер массива -> ");
 int size = int.Parse(Console.ReadLine());
 int[] nums = new int[size];
 int sum = 0;
-//var newRnd = new Random();
 for (int i = 0; i < size; i++)
 {
     nums[i] = new Random().Next(0, 1000);
@@ -16,7 +15,7 @@ for (int i = 0; i < size; i++)
 for (int i = 0; i < size; i++)
 {
 
-    if(nums[i] %2 == 0) sum += 1;
+    if (nums[i] % 2 == 0) sum += 1;
 }
 Console.WriteLine($" Количество чётных чисел в массиве = {sum}");
 
