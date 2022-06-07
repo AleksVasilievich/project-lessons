5045e93327874be6171e786467dc7726b7b73e74
AleksVasilievich <104069090+AleksVasilievich@users.noreply.github.com>
2022-06-07 12:01:20 +0300

    Add TSK_50

--- Task_50/Program.cs
+++ Task_50/Program.cs
@@ -22,25 +22,12 @@ void NumArray(int[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            if (m == i && n == j) num = array[i, j];
+            if (m == i && n == j) Console.WriteLine(array[i, j]);
         }
     }
-//return num;
+    //return num;
 }
 //Console.Write("Такого числа нет");
 NumArray(array);
 
 
-
-
-// void GetArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//              array[i,j] = new Random().Next(10);
-//         }
-//     }
-//     Console.Write(array);
-// }
