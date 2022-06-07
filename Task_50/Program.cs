1d9edd2ee075a5ce26d1e1a689d1647c28520f45
AleksVasilievich <104069090+AleksVasilievich@users.noreply.github.com>
2022-06-07 20:33:16 +0300

    Add Task_50

--- Task_50/Program.cs
+++ Task_50/Program.cs
@@ -15,17 +15,23 @@ Console.WriteLine("Введите позицию столбца n ->  ");
 int n = int.Parse(Console.ReadLine());
 
 int[,] array = new int[3, 4] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
-string num = string.Empty;
+//string num = string.Empty;
 void NumArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            //if (m != i && n != j) return;           //Console.WriteLine("Такого числа в массиве нет") ;
-            if(m == i && n == j) Console.Write(array[i, j]);
-               
-            
+            // //if (m != i && n != j) return;           //Console.WriteLine("Такого числа в массиве нет") ;
+            // if (m != i || n != j)
+            // {
+            //     Console.WriteLine("Такого числа в массиве нет"); //Console.Write(array[i, j]);
+            // }
+            if ((m == i && n == j))
+            {
+                Console.Write(array[i, j]);         //Console.WriteLine("Такого числа в массиве нет") ;
+            }
+
         }
 
     }
