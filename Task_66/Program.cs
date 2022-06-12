c6ede6a7f4ebacc960424ce3f29b7e0e723a35ab
AleksVasilievich <104069090+AleksVasilievich@users.noreply.github.com>
2022-06-13 00:36:56 +0300

    Add Task_66

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Enter a namber M -> ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Enter a namber N -> ");
+int n = int.Parse(Console.ReadLine());
+
+NumMN(m, n);
+
+int NumMN(int m, int n)
+{
+    if(m == n) return m;
+    if (m > n) return m + NumMN(m - 1, n);
+    else return m + NumMN(m + 1, n);
+}
+Console.Write(NumMN(m, n));
